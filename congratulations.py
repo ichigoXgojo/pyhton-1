5249ccd16e00382114cae72456dc37e2b98a13cd
ichigoXgojo <baijuevon37@gmail.com>
2025-08-14 06:52:08 -0700

    Add files via upload

---
+++ congratulations.py
@@ -0,0 +1,13 @@
+# A simple Python script to display a congratulatory message
+
+def congratulate(name):
+  """
+  Prints a personalized congratulatory message.
+  
+  Args:
+    name: The name of the person to congratulate.
+  """
+  print(f"Congratulations, {name}! You did an amazing job!")
+
+# Example usage:
+congratulate("Alex") 
