e6cfd504467b7599a59ed1a8da3b25ef4c715e51
ichigoXgojo <baijuevon37@gmail.com>
2025-08-16 20:26:29 +0530

    Add files via upload

---
+++ mirror triangle.py
@@ -0,0 +1,13 @@
+rows = 5 
+
+for i in range(1, rows + 1):
+
+    # Print spaces
+    for j in range(rows - i):
+        print(" ", end="")
+
+    # Print stars
+    for k in range(i):
+        print("*", end="")
+
+    print()
