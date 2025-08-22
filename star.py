252da9d3ef7461078284a19275cb8b88c4124286
ichigoXgojo <baijuevon37@gmail.com>
2025-08-22 20:59:30 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,27 @@
+import turtle 
+
+screen = turtle.Screen()
+screen.bgcolor("aqua")
+broad = turtle.Turtle()
+
+broad.forward(100)
+
+broad.left(120)
+broad.forward(100)
+
+broad.left(120)
+broad.forward(100)
+
+broad.penup()
+broad.right(150)
+broad.forward(50)
+
+broad.pendown()
+broad.right(90)
+broad.forward(100)
+
+broad.right(120)
+broad.forward(100)
+
+broad.right(120)
+broad.forward(100)
