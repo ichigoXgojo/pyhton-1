08c0b6af8d0fc435928fa244f10a1b4a41287d41
ichigoXgojo <baijuevon37@gmail.com>
2025-09-12 20:44:20 +0530

    Add files via upload

---
+++ random date and time time.py
@@ -0,0 +1,15 @@
+import random
+import time
+def getrandom_date(startdate, enddate ):
+    print("printing random date between", startdate, "and", enddate)
+    dateformat = '%d-%m-%Y'
+
+    starttime = time.mktime(time.strptime(startdate, dateformat))
+    endtime = time.mktime(time.strptime(enddate, dateformat))
+
+    randomtime = random.randint(starttime, endtime)
+    randomdate = time.strftime(dateformat, time.localtime(randomtime))
+    return randomdate
+
+print(random date ="getrandom_date("1-1-2016", "31-12-2018"))")
+
