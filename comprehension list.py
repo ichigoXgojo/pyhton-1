586cdf39c5c8df96b835de1b5c28fc77c1afa2cb
ichigoXgojo <baijuevon37@gmail.com>
2025-10-04 19:26:14 +0530

    Add files via upload

---
+++ comprehension list.py
@@ -0,0 +1,7 @@
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = [x**2 for x in numbers]
+# Result: [1, 4, 9, 16, 25]
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = [x for x in numbers if x % 2 == 0]
+# Result: [2, 4, 6, 8, 10]
