80057fd70223a9b72de958a3b2bd31a726c5eb95
ichigoXgojo <baijuevon37@gmail.com>
2025-10-11 20:27:58 +0530

    Add files via upload

---
+++ C2.py
@@ -0,0 +1,12 @@
+class vechile:
+    def __init__(self, max_speed, mileage):
+
+     self.max_speed = max_speed
+     self.mileage = mileage
+
+modelX = vechile(240, 18)
+
+print("max speed of modelX:", modelX.max_speed)
+print("mileage of modelX:", modelX.mileage)
+    
+
