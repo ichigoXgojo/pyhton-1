86c8338a9bdf5e544109a0ef93675d10706bd5c7
ichigoXgojo <baijuevon37@gmail.com>
2025-10-25 20:23:25 +0530

    Add files via upload

---
+++ ploymer.py
@@ -0,0 +1,20 @@
+class India():
+    def capital(self):
+        print("New Delhi is the capital of India.")
+    
+    def language(hindi):
+        print("Hindi is the most widely spoken language of India.")
+    
+    def type(self):
+        print("India is a developing country.")
+
+class Japan():
+    def capital(self):
+        print("Tokyo is the capital of Japan.")
+    
+    def language(self):
+        print("Japanese is the most widely spoken language of Japan.")
+    
+    def type(self):
+        print("Japan is a developed country.")
+        
