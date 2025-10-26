afc9c54814f8cfca9d6d3f963b2a24197ea87eeb
ichigoXgojo <baijuevon37@gmail.com>
2025-10-26 22:08:37 +0530

    Add files via upload

---
+++ supercar.py
@@ -0,0 +1,15 @@
+class Ferrari():
+    def type(self):
+        print("Ferrari is a high-performance racing car.")
+    
+    def max_speed(self):
+        print("The maximum speed of Ferrari is around 350 km/h.")
+
+class BNW ():
+    def type(self):
+        print("BNW is a luxury 3 box model car.")
+    
+    def max_speed(self):
+        print("The maximum speed of BNW is around 250 km/h.")
+     
+   
