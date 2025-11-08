2a280cf56bac58f45f2323bce5a2097a5562e86e
ichigoXgojo <baijuevon37@gmail.com>
2025-11-08 19:50:53 +0530

    Add files via upload

---
+++ pygame2.py
@@ -0,0 +1,14 @@
+import pygame  
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(30, 30, 60, 60))
+    
+    pygame.display.flip()
